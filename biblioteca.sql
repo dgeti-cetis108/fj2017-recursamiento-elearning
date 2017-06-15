9cecec06c4a59fb33fb27f38d3c64a4fc81c83d4
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-06-15 10:20:36 -0600

    commit inicial

---
+++ biblioteca.sql
@@ -0,0 +1,76 @@
+/*	Modelo de base de datos para biblioteca
+	ai->auto_increment | nn->not null | pk->primary key
+	vc->varchar | c->char | dt->datetime | df->default */
+drop schema if exists biblioteca;
+create schema if not exists biblioteca;
+/*
+Usuarios
+	id int ai pk,
+	nombre vc(50) nn,
+	apellidos vc(50) nn,
+	email vc(200) nn df "sin@correo" */
+
+/*
+Autores
+	id int ai pk,
+	nombre vc(50) nn,
+	apellidos vc(50) nn,
+	email vc(200) nn df "sin@correo" */
+create table autores (
+	id int auto_increment,
+	nombre varchar(50) not null,
+	apellidos varchar(50) not null,
+	email varchar(200) not null default "sin@correo",
+	primary key (id)
+) engine=innodb;
+
+/*
+Editoriales
+	id int ai pk,
+	nombre vc(200) nn,
+	direccion text nn,
+	email vc(200) nn df "sin@correo",
+	sitio_web vc(200) nn df "sin.sitio.web" */
+create table editoriales (
+	id int auto_increment,
+	nombre varchar(200) not null,
+	direccion text nn,
+	email varchar(200) not null default "sin@correo",
+	sitio_web varchar(200) not null default "sin.sitio.web",
+	primary key (id)
+) engine=innodb;
+
+/*
+Libros
+	id int ai pk,
+	titulo vc(200) nn,
+	paginas int nn,
+	autor_id int nn,
+	editorial_id int nn */
+create table libros (
+	id int auto_increment,
+	titulo varchar(200) not null,
+	paginas int not null,
+	autor_id int not null,
+	editorial_id int not null,
+	primary key (id),
+	constraint fk_libros_autores
+		foreign key (autor_id)
+		references autores (id)
+			on delete restrict
+			on update cascade,
+	constraint fk_libros_editoriales
+		foreign key (editorial_id)
+		references editoriales (id)
+			on delete restrict
+			on update cascade
+) engine=innodb;
+
+/*
+Prestamos
+	id int ai pk,
+	usuario_id int nn,
+	libro_id int nn,
+	fecha_entrega dt nn,
+	fecha_recepcion dt nn
+*/
